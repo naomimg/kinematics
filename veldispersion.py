76297ea13c4cdbd09a6e30be2fe1980c5105c2d4
Naomi McClure-Griffiths <naomi.mcclure-griffiths@anu.edu.au>
2017-06-01 09:25:57 +1000

    add python code

---
+++ veldispersion.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# N. McClure-Griffiths 31 May 2017
+
+from numpy import *
+from pylab import *
+from astropy.io import fits
+import os
+import scipy 
+from scipy.interpolate import interp1d
+
+import aplpy
+
+# Generic function to read the terminal velocities from the data
+def vterm(fname):
+    csv = np.genfromtxt (fname,comments='%')
+    l = csv[:,0]
+    v = csv[:,1]
+    for i in range(len(l)): 
+        if l[i]>180.0: l[i]-=360.
+    return np.array(l),np.array(v)
+
+
+# Now do a spline fit to the dv and dr values and return those new arrays
+#dv=map(lambda x: 50. if x>50. else x, dv) # Replace all values greater than 50 with 50.
+#dv=map(lambda x: 2. if x<2. else x, dv) # Replace all values less than 2 with 2
+#dv_int = interp1d(l, dv,bounds_error=False,fill_value=2.)
+#dr_int = interp1d(l, dr,kind='cubic',bounds_error=False,fill_value=1.)
+
+# open the FITS file
+input_cube='/Users/naomi/Data/GASS/GASS_GP.fits'
+hdulist = fits.open(input_cube)
+im = hdulist[0].data
+hdr = hdulist[0].header
+
+data_cube=1.*im
+dimensions = data_cube.shape
+
+idimensions=0
+if dimensions[0]==1:
+    idimensions=1        ## this means that the header most likely has a degenerate polarization axis
+
+nch=dimensions[0+idimensions]       ## number of velocity channels
+nypix=dimensions[1+idimensions]
+nxpix=dimensions[2+idimensions]
+
+vels=zeros(nch)
+
+# Read the terminal velocity values
+l1,v1=vterm('term_vel_q1.dat')   # First quadrant values
+l4,v4=vterm('term_vel_q4.dat')  # Fourth quadrant values
